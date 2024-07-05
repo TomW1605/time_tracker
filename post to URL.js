718d7e34811ba80b0845b72abe95db5257b3691a
Thomas White <tawhite@iinet.net.au>
2024-07-05 14:21:20 +0800

    add timeout to post to url js

--- post to URL.js
+++ post to URL.js
@@ -7,15 +7,28 @@ var url = args.urls[0]
 
 var request = new Request(url)
 
+request.timeoutInterval = 5;
 request.method = "post";
 request.body = JSON.stringify(data);
 request.headers = {"Content-Type":"application/json"}
 
-var json = await request.loadJSON()
-console.log(request.response.statusCode)
-console.log(json)
+try {
+  var json = await request.loadJSON()
+  console.log(request.response.statusCode)
+  console.log(json)
 
-return {
-        "statusCode": request.response.statusCode,
-        "body": json
+  return {"statusCode": request.response.statusCode,
+          "body": json}
+}
+catch(err) {
+  console.log(err.name)
+  console.log(err.message)
+  if (err.message=="The request timed out.") {
+    var status_code = "408"
+  } else {
+    var status_code = request.response.statusCode
+  }
+
+  return {"statusCode": status_code,
+          "body": ""}
 }
